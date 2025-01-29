adbe4b0c272bea5a28e21205e9144bb85ef0dca1
tester <mrj29496@gmail.com>
2025-01-30 01:56:48 +0330

    get ready to finish it up(AppCommond)

---
+++ Programs/AppCommond.cs
@@ -0,0 +1,13 @@
+﻿using Spectre.Console.Cli;
+using TeleCleanSlate.Common;
+
+namespace TeleCleanSlate.Programs
+{
+    internal class AppCommond : Command<Settings>
+    {
+        public override int Execute(CommandContext context, Settings settings)
+        {       
+            return 0;
+        }
+    }
+}
