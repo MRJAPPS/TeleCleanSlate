53e5c0e1aa7885f1d56a56f5ee0cf9ce10ce9bab
tester <mrj29496@gmail.com>
2025-01-31 19:42:37 +0330

    avoid unwanted AnsiConsole exceptions...

--- Common/AuthorizationHelper.cs
+++ Common/AuthorizationHelper.cs
@@ -7,22 +7,40 @@ namespace TeleCleanSlate.Common
 {
     internal static class AuthorizationHelper
     {
+        //For avoid unwanted AnsiConsole exceptions...
+        private readonly static object locker = new();
+
         public static void Helper_OnUnknownError(object s, Exception obj)
         {
-            ((AuthorizationHandler)s).Relase();
-            AnsiConsole.MarkupLine("[red]FATAL[/] : {0}", obj.Message);
-            Environment.Exit(3);
+            lock (locker)
+            {
+                ((AuthorizationHandler)s).Relase();
+                AnsiConsole.MarkupLine("[red]FATAL[/] : {0}", obj.Message);
+                Environment.Exit(3);
+            }
         }
 
         public static void Helper_OnError(object _, TdException obj)
         {
-            AnsiConsole.MarkupLine($"[red]{obj.Message}[/]");
-            if (obj.Message == nameof(CommonConstants.TdErrorCodes.API_ID_INVALID))
-                Environment.Exit(3);
+            lock (locker)
+            {
+                AnsiConsole.MarkupLine($"[red]{obj.Message}[/]");
+                if (obj.Message == nameof(CommonConstants.TdErrorCodes.API_ID_INVALID) || obj.Error.Code == CommonConstants.TdErrorCodes.UNREGISTERED)
+                    Environment.Exit(obj.Error.Code);
+
+            }
         }
 
-        public static string Helper_OnNeedCode(object _) => AnsiConsole.Prompt(new TextPrompt<string>("Enter the Code: ").Secret());
+        public static string Helper_OnNeedCode(object _)
+        {
+            lock (locker)
+                return AnsiConsole.Prompt(new TextPrompt<string>("Enter the Code: ").Secret());
+        }
 
-        public static string Helper_OnNeedPassword(object _) => AnsiConsole.Prompt(new TextPrompt<string>("Enter the password: ").Secret());
+        public static string Helper_OnNeedPassword(object _)
+        {
+            lock (locker)
+                return AnsiConsole.Prompt(new TextPrompt<string>("Enter the password: ").Secret());
+        }
     }
 }
